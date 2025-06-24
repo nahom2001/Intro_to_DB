9b1eeaf38c3f961776e3564fcc96cd21872265df
NahomH <nahomhulum@gmail.com>
2025-06-24 17:14:27 +0300

    database python connectivity

---
+++ MYSQLServer.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+alxdb = mysql.connector.connect(host='localhost', user='root', passwd='randompassword')
+
+dbcursor = alxdb.cursor()   
+
+try:
+    dbcursor.execute(" CREATE DATABASE IF NOT EXISTS alx_book_store")
+    print("Database 'alx_book_store' created successfully!")
+except:
+    print('Database connection failed!')
