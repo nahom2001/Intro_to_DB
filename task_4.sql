f3c45d8dc2b5b76b2a870fbbcda6a4d63390d94f
NahomH <nahomhulum@gmail.com>
2025-06-24 17:38:34 +0300

    passing table name in WHERE clause

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,8 @@
-SELECT *
-FROM alx_book_store.columns
+SELECT
+  COLUMN_NAME,
+  COLUMN_TYPE
+FROM 
+  INFORMATION_SCHEMA.COLUMNS
 WHERE 
   TABLE_NAME = 'Books'
   AND TABLE_SCHEMA = 'alx_book_store';
